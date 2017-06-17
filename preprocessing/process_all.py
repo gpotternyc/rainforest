872d335e9877c41c607a3ecf488ba928eb48a383
Justin Z <justinrz2018@gmail.com>
2017-06-17 05:12:55 -0400

    Initial commit

---
+++ preprocessing/process_all.py
@@ -0,0 +1,22 @@
+import glob, os, sys
+
+import numpy
+import PIL.Image
+
+import process
+
+if __name__ == "__main__":
+	if len(sys.argv) < 2:
+		print("Usage: python process_all.py [directory]")
+		sys.exit(1)
+	count = 1
+	for f in glob.glob(os.path.join(sys.argv[1], "*.jpg")):
+		directory = os.path.join(sys.argv[1], "processed")
+		if not os.path.isdir(directory):
+			os.mkdir(directory)
+		image = numpy.asarray(PIL.Image.open(f))
+		processed = PIL.Image.fromarray(process.process(image))
+		processed.save(os.path.join(directory, os.path.basename(f)))
+		if count % 100 == 0:
+			print("Finished {}".format(count))
+	print("Done!")
